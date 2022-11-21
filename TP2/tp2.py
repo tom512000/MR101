0ded3154a9ddaac48b528f8ddcbcad765e65cc87
TOM SIKORA <siko0001@2A4F1-31UPC128.ad-urca.univ-reims.fr>
2022-11-21 13:00:15 +0100

    Ajout de la fonction triBulles : tests OK

--- TP2/tp2.py
+++ TP2/tp2.py
@@ -5,3 +5,24 @@ def estTrie(liste: list) -> bool:
             bl = False
     return bl
 
+
+def echanger(liste: list, i1: int, i2: int) -> None:
+    a = liste[i1]
+    liste[i1] = liste[i2]
+    liste[i2] = a
+    return None
+
+
+def triBulles(liste: list) -> None:
+    etape = 1
+    modif = True
+    while modif:
+        modif = False
+        for i in range(len(liste)-1):
+            if liste[i] > liste[i+1]:
+                echanger(liste, i, i+1)
+                modif = True
+        etape += 1
+    return None
+
+
--- main_tp2.py
+++ main_tp2.py
@@ -1,9 +1,19 @@
 # main_tp2.py
 
 from TP1.tp1 import getRandomList
-
-from TP2.tp2 import estTrie
+from TP2.tp2 import *
 
 print(estTrie([1]))
 print(estTrie([2, 1]))
+
+a = [1, 2, 3, 4, 5, 6, 7]
+print(a)
+print(echanger(a, 1, 2))
+print(a)
+
+
+b = [0, -1, 8, 5, 21, -18, 2, 1]
+print(b)
+print(triBulles(b))
+print(b)
 
