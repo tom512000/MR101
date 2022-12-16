7dbe58c215b4249cdda28ce9a4e17811dd557f97
TOM SIKORA <siko0001@2A4F1-31UPC078.ad-urca.univ-reims.fr>
2022-12-16 10:37:59 +0100

    Ajout des fonctions getMin et getMax : tests OK

--- TP4/tp4.py
+++ TP4/tp4.py
@@ -14,10 +14,51 @@ def getRegularArray2D(nl: int, nc: int, mn: int, mx: int) -> list:
 def isRegular(lst: list) -> bool:
     a = True
     i = 0
-    while i != len(lst)-1 and a != False:
+    while i != len(lst) - 1 and a != False:
         if len(lst[i]) != len(lst[i + 1]):
             a = False
         i += 1
     return a
+
+
+def getMin(lst: list) -> int:
+    a = lst[0][0]
+    for i in range(len(lst)):
+        for j in range(len(lst[i])):
+            if lst[i][j] < a:
+                a = lst[i][j]
+    return a
+
+
+def getMax(lst: list) -> int:
+    a = lst[0][0]
+    for i in range(len(lst)):
+        for j in range(len(lst[i])):
+            if lst[i][j] > a:
+                a = lst[i][j]
+    return a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- main_tp4.py
+++ main_tp4.py
@@ -1,5 +1,6 @@
 # main_tp4.py
-from TP4.tp4 import getRegularArray2D
+from TP4.tp4 import *
+import random
 
 
 def test_getRegularArray2D() -> None:
@@ -8,4 +9,16 @@ def test_getRegularArray2D() -> None:
     return None
 
 
+print("\nFonction getRegularArray2D :")
 test_getRegularArray2D()
+
+
+def test_getMinMax() -> None:
+    for i in range(5):
+        lst = getRegularArray2D(random.randint(1, 5), random.randint(1, 5), -10, 10)
+        print(f"{lst} : Min = {getMin(lst)}, max = {getMax(lst)}")
+    return None
+
+
+print("\nFonction getMin et getMax :")
+test_getMinMax()
